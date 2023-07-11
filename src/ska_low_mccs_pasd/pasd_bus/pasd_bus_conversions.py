68cf14ea8184faf0370aa06406a6268c9f0200ec
Drew Devereux <drew.devereux@csiro.au>
2023-07-11 10:59:17 +0000

    [WOM-43] Simplify enumeration expression

--- src/ska_low_mccs_pasd/pasd_bus/pasd_bus_conversions.py
+++ src/ska_low_mccs_pasd/pasd_bus/pasd_bus_conversions.py
@@ -85,7 +85,7 @@ class PasdConversionUtility:
         if nbytes % 2:
             raise ValueError(f"Odd number of bytes to convert: {value_list}")
 
-        return sum(value_list[i] * (256 ** (nbytes - i - 1)) for i in range(nbytes))
+        return sum(value * (256 ** (nbytes - i - 1)) for i, value in enumerate(value_list))
 
     @classmethod
     def n_to_bytes(cls, value: int, nbytes: int = 2) -> list[int]:
