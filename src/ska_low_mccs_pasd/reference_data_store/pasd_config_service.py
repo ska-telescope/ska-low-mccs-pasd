7cea6b7f16a42decd764f8ffa052a3ae357e8709
Drew Devereux <drew.devereux@skao.int>
2024-04-08 00:31:25 +0000

    [MCCS-2091] separate server spec from referenceData

--- src/ska_low_mccs_pasd/reference_data_store/pasd_config_service.py
+++ src/ska_low_mccs_pasd/reference_data_store/pasd_config_service.py
@@ -24,44 +24,37 @@ from kubernetes.client import api_client
 class ConfigMapManager:
     """A class that manages access to a configMap."""
 
-    def __init__(
-        self: ConfigMapManager,
-        name: str,
-        namespace: str,
-        label_selector: str,
-    ) -> None:
+    def __init__(self: ConfigMapManager, namespace: str) -> None:
         """
         Manage read requests to a ConfigMap.
 
-        :param name: The name of the configmap to manage.
         :param namespace: The namespace the configmap resides
-        :param label_selector: a string with a key/value pair
-            e.g. "findme=true"
         """
-        self._configmap_name = name
         self._configmap_namespace = namespace
-        self._configmap_label = label_selector
 
         client = dynamic.DynamicClient(
             api_client.ApiClient(configuration=config.load_incluster_config())
         )
-        self._api = client.resources.get(api_version="v1", kind="ConfigMap")
+        self._configmap_view = client.resources.get(api_version="v1", kind="ConfigMap")
 
-    def read_data(self) -> dict:
+    def read_data(self, station: str) -> dict:
         """
         Read the data from the configmap.
+
+        :param station: The name of the station for which data is to be read.
+            The relevant configmaps are expected to be correctly labelled
+            e.g. "station=s8-1".
 
         :returns: a empty dictionary if unsuccessful
             or a dictionary containing data from configMap.
         """
-        configmap_list = self._api.get(
-            name=self._configmap_name,
+        configmaps = self._configmap_view.get(
             namespace=self._configmap_namespace,
-            label_selector=self._configmap_label,
+            label_selector=f"platform_spec=pasd, station={station}",
         )
-        config_data = configmap_list.data
-        for _, data_yaml in config_data.items():
-            data = yaml.safe_load(data_yaml)
+        configmap = next(iter(configmaps.items))
+        _, data_yaml = next(iter(configmap.data.items()))
+        data = yaml.safe_load(data_yaml)
         return data
 
 
@@ -72,25 +65,18 @@ class PasdConfigurationService:
     def __init__(
         self: PasdConfigurationService,
         namespace: str,
-        station_name: str,
         logger: logging.Logger,
-        _config_manager: Optional[Any] = None,
+        _config_manager: Optional[ConfigMapManager] = None,
     ) -> None:
         """
         Initialise a new instance.
 
         :param namespace: the namespace of the configMap.
-        :param station_name: the station name used to locate the configmap.
         :param logger: an injected logger for information.
         :param _config_manager: _config_manager to use for testing.
         """
         self.logger = logger
-        self._station_name = station_name
-        self.configmanager = _config_manager or ConfigMapManager(
-            name=f"pasd-configuration-{station_name}",
-            namespace=namespace,
-            label_selector="findme=configmap-to-watch",
-        )
+        self._configmanager = _config_manager or ConfigMapManager(namespace)
 
     def get_config(self: PasdConfigurationService, station_name: str) -> dict[str, Any]:
         """
@@ -100,16 +86,6 @@ class PasdConfigurationService:
             PaSD configuration.
 
         :return: configuration dictionary for the station
-
-        :raises PermissionError: if the station name requested doesn't match
-            the station served by this service.
         """
         self.logger.info(f"station name requesting {station_name} ...")
-        if not self._station_name == station_name:
-            self.logger.warning(f"access refused to station {self._station_name}")
-            raise PermissionError(
-                f"only {self._station_name} has permission to access configuration.",
-            )
-
-        self.logger.info(f"access granted to {self._station_name}")
-        return self.configmanager.read_data()
+        return self._configmanager.read_data(station_name)
