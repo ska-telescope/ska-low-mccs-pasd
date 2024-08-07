f0a6c50fc565ffba8856bb2048554ad7668cb0ea
Adam Clemens <ajc@observatorysciences.co.uk>
2024-08-07 15:19:46 +0000

    [MCCS-2217] Bump version in prep for REL-1607

--- src/ska_low_mccs_pasd/__init__.py
+++ src/ska_low_mccs_pasd/__init__.py
@@ -12,7 +12,7 @@ The Monitoring Control and Calibration (MCCS) subsystem is responsible
 for, amongst other things, monitoring and control of LFAA.
 """
 
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 
 __version_info__ = (
     "ska-low-mccs-pasd",
