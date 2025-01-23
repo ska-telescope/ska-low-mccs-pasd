e311caadca5b0dd7178912a1f681190e8a7d7c3b
Emma Arandjelovic <ela@observatorysciences.co.uk>
2025-01-23 12:51:21 +0000

    THORN-20: Add more adminMode tests.

--- tests/integration/test_admin_mode_propagation.py
+++ tests/integration/test_admin_mode_propagation.py
@@ -9,12 +9,9 @@
 
 from __future__ import annotations
 
-import time
-
 import pytest
 import tango
 from ska_control_model import AdminMode
-from ska_low_mccs_common.mode_inheritance import ModeInheritor
 from ska_tango_testing.mock.tango import MockTangoEventCallbackGroup
 
 
@@ -29,28 +26,87 @@ def change_event_callbacks_fixture() -> MockTangoEventCallbackGroup:
     return MockTangoEventCallbackGroup(
         "fndh_adminMode",
         "pasdbus_adminMode",
+        "fncc_adminMode",
+        "smartbox_adminMode",
         timeout=2.0,
         assert_no_error=False,
     )
 
 
-class TestAdminModePropagation:  # pylint: disable=too-few-public-methods
+class TestAdminModePropagation:
     """Test adminMode propagation."""
 
-    def test_admin_mode_propagation(
+    def test_fndh_mode_propagation(
         self: TestAdminModePropagation,
         field_station_device: tango.DeviceProxy,
         fndh_device: tango.DeviceProxy,
-        pasd_bus_device: tango.DeviceProxy,
         change_event_callbacks: MockTangoEventCallbackGroup,
     ) -> None:
         """
-        Test AdminMode propagation.
+        Test AdminMode propagation for FNDH.
 
         :param field_station_device: fixture that provides a
             :py:class:`tango.DeviceProxy` to the device under test, in a
             :py:class:`tango.test_context.DeviceTestContext`.
         :param fndh_device: fixture that provides a
+            :py:class:`tango.DeviceProxy` to the device under test, in a
+            :py:class:`tango.test_context.DeviceTestContext`.
+        :param change_event_callbacks: group of Tango change event
+            callback with asynchronous support
+        """
+        fndh_device.inheritModes = True
+
+        assert field_station_device.adminMode == AdminMode.OFFLINE
+        assert fndh_device.adminMode == AdminMode.OFFLINE
+
+        fndh_device.subscribe_event(
+            "adminMode",
+            tango.EventType.CHANGE_EVENT,
+            change_event_callbacks["fndh_adminMode"],
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+
+        # First change events might be subscription errors so we set
+        # a higher lookahead value
+        change_event_callbacks.assert_change_event(
+            "fndh_adminMode", AdminMode.ONLINE, lookahead=2, consume_nonmatches=True
+        )
+        assert fndh_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event("fndh_adminMode", AdminMode.OFFLINE)
+        assert fndh_device.adminMode == AdminMode.OFFLINE
+
+        # Turn inheritance off by setting directly
+        fndh_device.adminMode = AdminMode.ENGINEERING
+        change_event_callbacks.assert_change_event(
+            "fndh_adminMode", AdminMode.ENGINEERING
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+        change_event_callbacks["fndh_adminMode"].assert_not_called()
+        assert fndh_device.adminMode == AdminMode.ENGINEERING
+
+        # Go back to inheriting
+        fndh_device.inheritModes = True
+        change_event_callbacks.assert_change_event("fndh_adminMode", AdminMode.ONLINE)
+        assert fndh_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event("fndh_adminMode", AdminMode.OFFLINE)
+        assert fndh_device.adminMode == AdminMode.OFFLINE
+
+    def test_pasdbus_mode_propagation(
+        self: TestAdminModePropagation,
+        field_station_device: tango.DeviceProxy,
+        pasd_bus_device: tango.DeviceProxy,
+        change_event_callbacks: MockTangoEventCallbackGroup,
+    ) -> None:
+        """
+        Test AdminMode propagation for pasdbus.
+
+        :param field_station_device: fixture that provides a
             :py:class:`tango.DeviceProxy` to the device under test, in a
             :py:class:`tango.test_context.DeviceTestContext`.
         :param pasd_bus_device: fixture that provides a
@@ -59,20 +115,10 @@ class TestAdminModePropagation:  # pylint: disable=too-few-public-methods
         :param change_event_callbacks: group of Tango change event
             callback with asynchronous support
         """
-        # Wait for the ModeInheritor initial thread to finish so that
-        # the initial value of the adminMode attribute is set.
-        # TODO: Move this to a fixture.
-        time.sleep(ModeInheritor.INITIAL_VALUE_TIMEOUT + 1)
+        pasd_bus_device.inheritModes = True
 
         assert field_station_device.adminMode == AdminMode.OFFLINE
-        assert fndh_device.adminMode == AdminMode.OFFLINE
         assert pasd_bus_device.adminMode == AdminMode.OFFLINE
-
-        fndh_device.subscribe_event(
-            "adminMode",
-            tango.EventType.CHANGE_EVENT,
-            change_event_callbacks["fndh_adminMode"],
-        )
 
         pasd_bus_device.subscribe_event(
             "adminMode",
@@ -82,11 +128,166 @@ class TestAdminModePropagation:  # pylint: disable=too-few-public-methods
 
         field_station_device.adminMode = AdminMode.ONLINE
 
-        change_event_callbacks.assert_change_event(
-            "fndh_adminMode", AdminMode.ONLINE, lookahead=4, consume_nonmatches=True
-        )
+        # First change events might be subscription errors so we set
+        # a higher lookahead value
         change_event_callbacks.assert_change_event(
             "pasdbus_adminMode", AdminMode.ONLINE, lookahead=2, consume_nonmatches=True
         )
-        assert fndh_device.adminMode == AdminMode.ONLINE
         assert pasd_bus_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event(
+            "pasdbus_adminMode", AdminMode.OFFLINE
+        )
+        assert pasd_bus_device.adminMode == AdminMode.OFFLINE
+
+        # Turn inheritance off by setting directly
+        pasd_bus_device.adminMode = AdminMode.ENGINEERING
+        change_event_callbacks.assert_change_event(
+            "pasdbus_adminMode", AdminMode.ENGINEERING
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+        change_event_callbacks["pasdbus_adminMode"].assert_not_called()
+        assert pasd_bus_device.adminMode == AdminMode.ENGINEERING
+
+        # Go back to inheriting
+        pasd_bus_device.inheritModes = True
+        change_event_callbacks.assert_change_event(
+            "pasdbus_adminMode", AdminMode.ONLINE
+        )
+        assert pasd_bus_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event(
+            "pasdbus_adminMode", AdminMode.OFFLINE
+        )
+        assert pasd_bus_device.adminMode == AdminMode.OFFLINE
+
+    def test_smartbox_mode_propagation(
+        self: TestAdminModePropagation,
+        field_station_device: tango.DeviceProxy,
+        on_smartbox_device: tango.DeviceProxy,
+        change_event_callbacks: MockTangoEventCallbackGroup,
+    ) -> None:
+        """
+        Test AdminMode propagation for Smartbox.
+
+        :param field_station_device: fixture that provides a
+            :py:class:`tango.DeviceProxy` to the device under test, in a
+            :py:class:`tango.test_context.DeviceTestContext`.
+        :param on_smartbox_device: fixture that provides a
+            :py:class:`tango.DeviceProxy` to the device under test, in a
+            :py:class:`tango.test_context.DeviceTestContext`.
+        :param change_event_callbacks: group of Tango change event
+            callback with asynchronous support
+        """
+        on_smartbox_device.inheritModes = True
+
+        assert field_station_device.adminMode == AdminMode.OFFLINE
+        assert on_smartbox_device.adminMode == AdminMode.OFFLINE
+
+        on_smartbox_device.subscribe_event(
+            "adminMode",
+            tango.EventType.CHANGE_EVENT,
+            change_event_callbacks["smartbox_adminMode"],
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+
+        # First change events might be subscription errors so we set
+        # a higher lookahead value
+        change_event_callbacks.assert_change_event(
+            "smartbox_adminMode", AdminMode.ONLINE, lookahead=2, consume_nonmatches=True
+        )
+        assert on_smartbox_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event(
+            "smartbox_adminMode", AdminMode.OFFLINE
+        )
+        assert on_smartbox_device.adminMode == AdminMode.OFFLINE
+
+        # Turn inheritance off by setting directly
+        on_smartbox_device.adminMode = AdminMode.ENGINEERING
+        change_event_callbacks.assert_change_event(
+            "smartbox_adminMode", AdminMode.ENGINEERING
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+        change_event_callbacks["smartbox_adminMode"].assert_not_called()
+        assert on_smartbox_device.adminMode == AdminMode.ENGINEERING
+
+        # Go back to inheriting
+        on_smartbox_device.inheritModes = True
+        change_event_callbacks.assert_change_event(
+            "smartbox_adminMode", AdminMode.ONLINE
+        )
+        assert on_smartbox_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event(
+            "smartbox_adminMode", AdminMode.OFFLINE
+        )
+        assert on_smartbox_device.adminMode == AdminMode.OFFLINE
+
+    def test_fncc_mode_propagation(
+        self: TestAdminModePropagation,
+        field_station_device: tango.DeviceProxy,
+        fncc_device: tango.DeviceProxy,
+        change_event_callbacks: MockTangoEventCallbackGroup,
+    ) -> None:
+        """
+        Test AdminMode propagation for Smartbox.
+
+        :param field_station_device: fixture that provides a
+            :py:class:`tango.DeviceProxy` to the device under test, in a
+            :py:class:`tango.test_context.DeviceTestContext`.
+        :param fncc_device: fixture that provides a
+            :py:class:`tango.DeviceProxy` to the device under test, in a
+            :py:class:`tango.test_context.DeviceTestContext`.
+        :param change_event_callbacks: group of Tango change event
+            callback with asynchronous support
+        """
+        fncc_device.inheritModes = True
+
+        assert field_station_device.adminMode == AdminMode.OFFLINE
+        assert fncc_device.adminMode == AdminMode.OFFLINE
+
+        fncc_device.subscribe_event(
+            "adminMode",
+            tango.EventType.CHANGE_EVENT,
+            change_event_callbacks["fncc_adminMode"],
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+
+        # First change events might be subscription errors so we set
+        # a higher lookahead value
+        change_event_callbacks.assert_change_event(
+            "fncc_adminMode", AdminMode.ONLINE, lookahead=2, consume_nonmatches=True
+        )
+        assert fncc_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event("fncc_adminMode", AdminMode.OFFLINE)
+        assert fncc_device.adminMode == AdminMode.OFFLINE
+
+        # Turn inheritance off by setting directly
+        fncc_device.adminMode = AdminMode.ENGINEERING
+        change_event_callbacks.assert_change_event(
+            "fncc_adminMode", AdminMode.ENGINEERING
+        )
+
+        field_station_device.adminMode = AdminMode.ONLINE
+        change_event_callbacks["fncc_adminMode"].assert_not_called()
+        assert fncc_device.adminMode == AdminMode.ENGINEERING
+
+        # Go back to inheriting
+        fncc_device.inheritModes = True
+        change_event_callbacks.assert_change_event("fncc_adminMode", AdminMode.ONLINE)
+        assert fncc_device.adminMode == AdminMode.ONLINE
+
+        field_station_device.adminMode = AdminMode.OFFLINE
+        change_event_callbacks.assert_change_event("fncc_adminMode", AdminMode.OFFLINE)
+        assert fncc_device.adminMode == AdminMode.OFFLINE
