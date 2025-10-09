d9f160732deb28e6f8e81e8dd2a4b1a8ba9009f0
Emma Arandjelovic <ela@observatorysciences.co.uk>
2025-10-09 14:31:54 +0100

    THORN-253: Remove unused function

--- src/ska_low_mccs_pasd/pasd_utils.py
+++ src/ska_low_mccs_pasd/pasd_utils.py
@@ -7,53 +7,6 @@
 # See LICENSE for more info.
 """Module provides common code for all PaSD devices."""
 
-from logging import Logger
-
-from tango import Attribute
-
-
-def configure_alarms(
-    attribute: Attribute, alarm_values: list[float], logger: Logger
-) -> None:
-    """Configure the alarm properties for a Tango attribute.
-
-    :param attribute: Tango Attribute to configure
-    :param alarm_values: list of alarm threshold values in the order:
-        max_alarm; max_warning; min_warning; min_alarm
-    :param logger: Logger object to log warning messages with
-    """
-    multi_prop = attribute.get_properties()
-    # Threshold values always reported in the order:
-    # high alarm - high warning - low warning - low alarm
-    if min(alarm_values[0], alarm_values[1]) <= max(alarm_values[2], alarm_values[3]):
-        logger.warning(
-            f"Invalid alarm configuration detected for {attribute.get_name()}: "
-            f"[{alarm_values[0]}, {alarm_values[1]}, "
-            f"{alarm_values[2]}, {alarm_values[3]}]"
-        )
-        return
-
-    if alarm_values[0] <= alarm_values[1]:
-        logger.warning(
-            f"High alarm value {alarm_values[0]} for {attribute.get_name()} "
-            f"is <= warning value {alarm_values[1]}"
-        )
-        multi_prop.max_alarm = max(alarm_values[0], alarm_values[1])
-    else:
-        multi_prop.max_alarm = alarm_values[0]
-        multi_prop.max_warning = alarm_values[1]
-
-    if alarm_values[2] <= alarm_values[3]:
-        logger.warning(
-            f"Low warning value {alarm_values[2]} for {attribute.get_name()} "
-            f"is <= alarm value {alarm_values[3]}"
-        )
-        multi_prop.min_alarm = min(alarm_values[2], alarm_values[3])
-    else:
-        multi_prop.min_warning = alarm_values[2]
-        multi_prop.min_alarm = alarm_values[3]
-    attribute.set_properties(multi_prop)
-
 
 def join_health_reports(messages: list[str]) -> str:
     """
