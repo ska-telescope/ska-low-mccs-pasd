17ae4e06cac4a6f188b8da3e1664a73528b673ec
Emma Arandjelovic <ela@observatorysciences.co.uk>
2026-01-22 09:45:57 +0800

    SKB-1171: Prepare for release

--- src/ska_low_mccs_pasd/__init__.py
+++ src/ska_low_mccs_pasd/__init__.py
@@ -12,7 +12,7 @@ The Monitoring Control and Calibration (MCCS) subsystem is responsible
 for, amongst other things, monitoring and control of LFAA.
 """
 
-__version__ = "4.1.0"
+__version__ = "4.2.0"
 
 __version_info__ = (
     "ska-low-mccs-pasd",
